272c85226a7679e1dfc64758d132fa6fc6b67977
Michael Shih <45140812+Mikerpen22@users.noreply.github.com>
2026-04-18 20:34:21 -0700

    Add: single-threaded-cpu - submission-1

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-1.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+
+        tasks = [(task[0], task[1], i) for i, task in enumerate(tasks)]
+        tasks.sort(key=lambda x: x[0])  # sort by enqueue time
+        time = 0
+        i = 0
+        res = []
+        processing = []  # minheap
+
+        if tasks:
+            time = tasks[0][0]
+
+        while i < len(tasks) or processing:
+            # move eligible tasks to heap
+            while i < len(tasks) and tasks[i][0] <= time:
+                enqueue_t, process_t, idx = tasks[i]
+                heapq.heappush(processing, (process_t, idx, enqueue_t))
+                i += 1
+
+            # process the first task from the minheap
+            if processing:
+                proc_t, idx, enq_t = heapq.heappop(processing)
+                time += proc_t
+                res.append(idx)
+            else:
+                # no task is avaialbe at this time, fastforward time
+                if i < len(tasks):
+                    time = tasks[i][0]
+
+        return res
