259dc0dba9df33f3894fabce3e4718c67d8380a4
Michael Shih <45140812+Mikerpen22@users.noreply.github.com>
2026-05-03 21:06:14 -0700

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # prefix sum
+        psum = defaultdict(int)
+        psum[0] = 1
+        res = 0 
+        running_sum = 0
+
+        for num in nums:
+            running_sum += num
+            # if (running_sum - k) in psum that means how many answer we can add
+            res += psum[running_sum - k]
+            psum[running_sum] += 1
+        return res
+
+
+
+
+
+        
+
+
