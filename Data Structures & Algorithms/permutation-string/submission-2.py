e9529af0ab31449cc08862386c9fa4a277008805
Michael Shih <45140812+Mikerpen22@users.noreply.github.com>
2026-07-15 21:52:41 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2): return False
+
+        seenS1 = [0] * 26
+        seenS2 = [0] * 26
+        for i, c in enumerate(s1):
+            seenS1[ord(c) - ord('a')] += 1
+            seenS2[ord(s2[i]) - ord('a')] += 1
+        
+
+        # two pointer sliding window (for seenS2)
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if seenS1 == seenS2: 
+                return True
+            
+            seenS2[ord(s2[r]) - ord('a')] += 1
+            seenS2[ord(s2[l]) - ord('a')] -= 1
+            l += 1
+
+        return seenS1 == seenS2
